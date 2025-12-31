e127a537d7d1b2278de39e3392e638a3e4f94a65
Duong Pham Trong Nghia <t.nghia2112279@gmail.com>
2025-12-31 13:31:27 +0700

    #2 - Css nha

--- 02-design-pattern/src/components/learn/MyComponent.jsx
+++ 02-design-pattern/src/components/learn/MyComponent.jsx
@@ -8,15 +8,25 @@
       + for -> htmlFor
       + style={{ color: "red", fontSize: "14px" }} (object)
     - Sử dụng được Hooks (useState, useEffect, ...)
-    - Chỉ trả về những thứ trong () của return
+    - Chỉ trả về những thứ 
+        - Trong () của return
+        - 1 tag duy nhất dùng Fragment (mảnh vỡ) or div
+    - Vậy thì dùng div hay Fragment ?
+      + div: thêm 1 thẻ div vào trong DOM (có thể ảnh hưởng đến CSS, layout)
+      + Fragment: không thêm thẻ gì vào trong DOM (chỉ là mảnh vỡ vô hình)
+      => Tùy theo layout mà bạn chọn div hay Fragment
+      để chia giao diện
 
 */
+import "./style.css";
 
 const MyComponent = () => {
   return (
     <>
-      <h2>This is my component</h2>
-      <p>Welcome to learning React!</p>
+      <h2 style={{ color: "hotpink", fontSize: "14px" }}>
+        This is my component
+      </h2>
+      <p className="child">Welcome to learning React!</p>
     </>
   );
 };
