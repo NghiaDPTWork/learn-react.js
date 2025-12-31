a7d8c53c8e9bc7d7630be18aa8c6c5e24737932b
Duong Pham Trong Nghia <t.nghia2112279@gmail.com>
2025-12-31 13:51:30 +0700

    #2 - Bien so voi JSX

--- 02-design-pattern/src/main.jsx
+++ 02-design-pattern/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /*
+    Với React.StrictMode, các thành phần con sẽ được kiểm tra kỹ lưỡng hơn (render 2 lần)
+    trong quá trình phát triển. Điều này giúp phát hiện các vấn đề tiềm ẩn trong ứng dụng của bạn.
+    Lưu ý: Chỉ sử dụng StrictMode trong môi trường phát triển, không sử dụng trong môi trường sản xuất.
+  */
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
