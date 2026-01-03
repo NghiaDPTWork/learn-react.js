7f44a7239751304b006cbf7aba05bc513af582a1
Duong Pham Trong Nghia <t.nghia2112279@gmail.com>
2026-01-03 19:37:32 +0700

    #3 - Tránh reload với CSR - link

--- 03-router-in-react/src/components/layout/Header/Header.jsx
+++ 03-router-in-react/src/components/layout/Header/Header.jsx
@@ -12,7 +12,19 @@ const Navbar = () => {
           <li>
             <Link to="/">Home</Link>
           </li>
+          {/* 
+            Việc sử dụng thẻ <a> như này là sẽ làm performance giảm 
+            => Buộc phải reload trang đó là cơ chế của HTML
+            Để tránh tình trạng này thì REACT đã sinh ra một khái niệm 
+            là Client Side Routing (CSR) -> là kỹ thuật định tuyến (chuyển trang) 
+            được xử lý hoàn toàn bằng JavaScript ngay trong trình 
+            duyệt của người dùng, không cần tải lại toàn bộ trang từ máy chủ
+
+            Và nó cho mình component tên là Link => tránh reload trang 
+            => 1 trong các yếu tố gọi là SPA (Single Page Application)
+           */}
           <li>
+            {/* <a href="/users">Users</a> */}
             <Link to="/users">Users</Link>
           </li>
           <li>
