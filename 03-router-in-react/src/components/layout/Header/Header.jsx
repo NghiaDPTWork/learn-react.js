365d720c553c7648e28cd6582d1db86d04b55fe6
Duong Pham Trong Nghia <t.nghia2112279@gmail.com>
2026-01-03 19:44:20 +0700

    #3 - Tự style khi ấn vào với NavLink

--- 03-router-in-react/src/components/layout/Header/Header.jsx
+++ 03-router-in-react/src/components/layout/Header/Header.jsx
@@ -1,16 +1,16 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand">
+        <NavLink to="/" className="navbar-brand">
           My App
-        </Link>
+        </NavLink>
         <ul className="navbar-menu">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/">Home</NavLink>
           </li>
           {/* 
             Việc sử dụng thẻ <a> như này là sẽ làm performance giảm 
@@ -20,21 +20,25 @@ const Navbar = () => {
             được xử lý hoàn toàn bằng JavaScript ngay trong trình 
             duyệt của người dùng, không cần tải lại toàn bộ trang từ máy chủ
 
-            Và nó cho mình component tên là Link => tránh reload trang 
+            Và nó cho mình component tên là NavLink => tránh reload trang 
             => 1 trong các yếu tố gọi là SPA (Single Page Application)
+
+            NavLink về bản chất là giống link tuy nhiên nó sẽ tự động thêm class
+            active vào các ptu trong navbar nếu ấn 
+            vào thì có hiệu ứng => tránh thêm thủ công
            */}
           <li>
             {/* <a href="/users">Users</a> */}
-            <Link to="/users">Users</Link>
+            <NavLink to="/users">Users</NavLink>
           </li>
           <li>
-            <Link to="/product">Product</Link>
+            <NavLink to="/product">Product</NavLink>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <NavLink to="/login">Login</NavLink>
           </li>
           <li>
-            <Link to="/register">Register</Link>
+            <NavLink to="/register">Register</NavLink>
           </li>
         </ul>
       </div>
